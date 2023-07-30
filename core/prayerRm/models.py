d90e512bc81b6889a9e66edd2e16ebf3c5f9dfe7
Clement Idemudo <clemcy9@gmail.com>
2023-07-30 01:38:24 +0100

    added prayerRm to installed apps

--- core/prayerRm/models.py
+++ core/prayerRm/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+user = get_user_model()
 
 class PrayerRm(models.Model):
     name = models.CharField(max_length=255)
-    participants = models.ManyToManyField(, null=True, blank=True)
-    admin = models.ForeignKey()
+    participants = models.ManyToManyField(user)
+    admin = models.ForeignKey(user,related_name='admin_group', on_delete=models.DO_NOTHING, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
     
 class Chats(models.Model):
-    user = models.OneToOneField()
+    user = models.ForeignKey(user, on_delete=models.DO_NOTHING, null=True, blank=True)
     message = models.TextField()
     group = models.ForeignKey(PrayerRm, on_delete=models.DO_NOTHING,null=True, blank=True)
     time_stamp = models.DateTimeField(auto_now_add=True)
