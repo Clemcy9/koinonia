2dc560bd531aa8b9b704d7e17264cd3d38371d67
Clement Idemudo <clemcy9@gmail.com>
2023-08-03 07:15:23 +0100

    done community model

--- core/community/models.py
+++ core/community/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Community(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    members = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True, blank=True)
+    admins =  models.ForeignKey(settings.AUTH_USER_MODEL, related_name='admin',on_delete=models.DO_NOTHING, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.name
--- core/prayerRm/models.py
+++ core/prayerRm/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.conf import settings
 # Create your models here.
 user = get_user_model()
 class PrayerRm(models.Model):
@@ -11,7 +13,7 @@ class PrayerRm(models.Model):
         return self.name
     
 class Chats(models.Model):
-    user = models.ForeignKey(user, on_delete=models.DO_NOTHING, null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True, blank=True)
     message = models.TextField()
     group = models.ForeignKey(PrayerRm, on_delete=models.DO_NOTHING,null=True, blank=True)
     time_stamp = models.DateTimeField(auto_now_add=True)
